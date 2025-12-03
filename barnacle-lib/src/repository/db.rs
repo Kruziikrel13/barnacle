fe90f7b34062a8607f06a2775dbcefcdb9be7c90
PopeRigby <poperigby@mailbox.org>
2025-12-02 21:55:22 -0800

    lib: cleanup db stuff, add backup fn

--- barnacle-lib/src/repository/db.rs
+++ barnacle-lib/src/repository/db.rs
@@ -19,36 +19,49 @@ impl DbHandle {
     pub fn new() -> Self {
         let path = state_dir().join("data.db");
         let path_str = path.to_str().unwrap();
-        Self::init(DbAny::new_file(path_str).unwrap())
+
+        let mut db = Self {
+            db: Arc::new(RwLock::new(DbAny::new_file(path_str).unwrap())),
+        };
+
+        db.init();
+
+        db
     }
 
-    fn init(mut db: DbAny) -> Self {
+    fn init(&mut self) {
         // Insert aliases if they don't exist
-        if db
+        if self
+            .db
+            .read()
             .exec(QueryBuilder::select().aliases().query())
             .unwrap()
             .result
             == 0
         {
-            db.exec_mut(
-                QueryBuilder::insert()
-                    .nodes()
-                    .aliases([
-                        "games",
-                        "profiles",
-                        "mods",
-                        "tools",
-                        // State
-                        "current_profile",
-                        "model_version",
-                    ])
-                    .query(),
-            )
-            .unwrap();
+            self.db
+                .write()
+                .exec_mut(
+                    QueryBuilder::insert()
+                        .nodes()
+                        .aliases([
+                            "games",
+                            "profiles",
+                            "mods",
+                            "tools",
+                            // State
+                            "current_profile",
+                            "model_version",
+                        ])
+                        .query(),
+                )
+                .unwrap();
         }
 
         // Fetch the current model version (if any)
-        let result = db
+        let result = self
+            .db
+            .read()
             .exec(
                 QueryBuilder::select()
                     .elements::<ModelVersion>()
@@ -64,45 +77,62 @@ impl DbHandle {
 
         if let Some(mv) = model_version {
             if mv.version() < CURRENT_MODEL_VERSION {
-                // TODO: perform migrations
-                // db.backup(filename)
-                dbg!(mv);
+                self.backup();
+                self.migrate();
             }
         } else {
             // Insert default ModelVersion if missing
-            db.transaction_mut(|t| -> Result<(), agdb::DbError> {
-                let model_version_id = t
-                    .exec_mut(
+            self.db
+                .write()
+                .transaction_mut(|t| -> Result<(), agdb::DbError> {
+                    let model_version_id = t
+                        .exec_mut(
+                            QueryBuilder::insert()
+                                .element(ModelVersion::default())
+                                .query(),
+                        )?
+                        .elements
+                        .first()
+                        .unwrap()
+                        .id;
+
+                    t.exec_mut(
                         QueryBuilder::insert()
-                            .element(ModelVersion::default())
+                            .edges()
+                            .from("model_version")
+                            .to(model_version_id)
                             .query(),
-                    )?
-                    .elements
-                    .first()
-                    .unwrap()
-                    .id;
+                    )?;
 
-                t.exec_mut(
-                    QueryBuilder::insert()
-                        .edges()
-                        .from("model_version")
-                        .to(model_version_id)
-                        .query(),
-                )?;
-
-                Ok(())
-            })
-            .unwrap();
+                    Ok(())
+                })
+                .unwrap();
         }
+    }
 
-        Self {
-            db: Arc::new(RwLock::new(db)),
-        }
+    /// Perform a backup of the database
+    fn backup(&self) {
+        let timestamp = chrono::Local::now().format("%Y%m%d-%H%M%S");
+        let path = state_dir().join(format!("data-{}.db.bak", timestamp));
+        let path_str = path.to_str().unwrap();
+
+        self.db.write().backup(path_str).unwrap();
+    }
+
+    /// Perform database migrations
+    fn migrate(&self) {
+        todo!()
     }
 
     /// Create a memory backed database for use in tests
     #[cfg(test)]
     pub(crate) fn in_memory() -> Self {
-        Self::init(DbAny::new_memory("data.db").unwrap())
+        let mut db = Self {
+            db: Arc::new(RwLock::new(DbAny::new_memory("test").unwrap())),
+        };
+
+        db.init();
+
+        db
     }
 }
