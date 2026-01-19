7f73815873b54985e1f3dd1e02956c70bfad83b6
kruziikrel13 <dev@michaelpetersen.io>
2026-01-19 11:47:15 +1000

    Revert "use devenv for developer environment"
    This reverts commit 2f34bcd76f78af28bbefb53caf9a7aaa5f663be8.

--- flake.nix
+++ flake.nix
@@ -16,11 +16,54 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        naersk-lib = callPackage naersk { };
 
-        naersk-lib = pkgs.callPackage naersk { };
+        inherit (pkgs)
+          mkShell
+          rustPlatform
+          callPackage
+          lib
+          ;
       in
       {
         defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = mkShell {
+          packages = with pkgs; [
+            # Tools
+            bacon
+            cargo
+            diesel-cli
+            cargo-info
+            rustPackages.clippy
+            rustfmt
+
+            # Dependencies
+            fuse-overlayfs
+            libarchive
+            openssl
+            pkg-config
+            rustc
+
+            # Slint
+            fontconfig
+            xorg.libxcb
+            wayland
+            libxkbcommon
+            libGL
+          ];
+          env = {
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${
+              with pkgs;
+              lib.makeLibraryPath [
+                wayland
+                libxkbcommon
+                fontconfig
+                libGL
+              ]
+            }";
+          };
+        };
       }
     );
 }
